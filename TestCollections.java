5f58bc9c05aa8a9e31216cefe1a290948a566470
Ernesto Corbellini <ecorbellini@ekumenlabs.com>
2017-02-24 15:32:22 -0300

    Added java collection tests.

--- TestCollections.java
+++ TestCollections.java
@@ -6,16 +6,14 @@ import java.util.Arrays;
 class TestCollections {
     
   interface Filter<T> {
-      public boolean evaluate(T element);
+    boolean evaluate(T element);
   }
-  
-  class FilterList<T> {
-      private Filter evaluator;
       
+  class FilterList<T> {
+      private Filter<T> evaluator;
       public FilterList(Filter evaluator) {
           this.evaluator = evaluator;
       }
-      
       public List<T> filter(List<T> inputList) {
           List<T> filteredList = new ArrayList<>();
           for (T element : inputList) {
@@ -27,6 +25,42 @@ class TestCollections {
       }
   }
   
+  interface Remapper<T> {
+      T remap(T element);
+  }
+  
+  class RemapList<T> {
+      private Remapper<T> mapper;
+      public RemapList(Remapper mapper) {
+          this.mapper = mapper;
+      }
+      public List<T> remap(List<T> inputList) {
+          List<T> remappedList = new ArrayList<>();
+          for (T element : inputList) {
+              remappedList.add(mapper.remap(element));
+          }
+          return remappedList;
+      }
+  }
+  
+  interface Reducer<T> {
+      T reduce(T accumulator, T element);
+  }
+  
+  class ReduceList<T> {
+      private Reducer<T> reducer;
+      public ReduceList(Reducer reducer) {
+          this.reducer = reducer;
+      }
+      public T reduce(T initial, List<T> inputList) {
+          T accumulator = initial;
+          for (T element : inputList) {
+              accumulator = reducer.reduce(accumulator, element);
+          }
+          return accumulator;
+      }
+  }
+  
   public static void main(String[] args) {
     System.out.println("Testing collections in Java.");
     TestCollections app = new TestCollections();
@@ -35,7 +69,7 @@ class TestCollections {
 
   private void test() {
     List<Integer> myList = new ArrayList(Arrays.asList(10, 3, 5, 8, 7));
-    System.out.println(myList);
+    System.out.println("Original list: " + myList);
     
     System.out.println("first: " + myList.get(0));
     System.out.println("last: " + myList.get(myList.size() - 1));
@@ -45,11 +79,32 @@ class TestCollections {
     System.out.println("includes 5?: " + myList.contains(5));
     System.out.println("includes 23?: " + myList.contains(23));
     FilterList<Integer> myFilter = new FilterList(new Filter<Integer>() {
-            public boolean evaluate(Integer element) {
-                return (element > 5);
-            }
-        });
+        public boolean evaluate(Integer element) {
+            return (element > 5);
+        }
+    });
     List<Integer> filteredList = myFilter.filter(myList);
     System.out.println("elements greater than 5: " + filteredList);
+    RemapList<Integer> myMapper = new RemapList(new Remapper<Integer>() {
+        public Integer remap(Integer element) {
+            return Integer.valueOf(element.intValue() + 1);
+        }
+    });
+    List<Integer> remappedList = myMapper.remap(myList);
+    System.out.println("+1 to each element: " + remappedList);
+    ReduceList<Integer> mySumReducer = new ReduceList<>(new Reducer<Integer>() {
+        public Integer reduce(Integer acc, Integer element) {
+            return Integer.valueOf(acc.intValue() + element.intValue());
+        }
+    });
+    Integer sum = mySumReducer.reduce(Integer.valueOf(0), myList);
+    System.out.println("sum all elements: " + sum.intValue());
+    ReduceList<Integer> myMultReducer = new ReduceList<>(new Reducer<Integer>() {
+        public Integer reduce(Integer acc, Integer element) {
+            return Integer.valueOf(acc.intValue() * element.intValue());
+        }
+    });
+    Integer mult = myMultReducer.reduce(Integer.valueOf(1), myList);
+    System.out.println("multiply all elements: " + mult.intValue());
   }
 }
